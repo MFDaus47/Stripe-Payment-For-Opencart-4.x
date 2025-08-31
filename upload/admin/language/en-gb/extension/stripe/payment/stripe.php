08b94986cab3ddf25eeb1f3a72ff30b2c9c6115f
MFDaus47 <mfdaus3158@gmail.com>
2025-08-31 19:14:50 +0800

    moved payment folder under stripe folder according to opencart 4.x extension requirement. new namespace: admin/controller/extension/stripe/payment

---
+++ upload/admin/language/en-gb/extension/stripe/payment/stripe.php
@@ -0,0 +1,39 @@
+<?php
+// Heading
+$_['heading_title'] = 'Stripe';
+
+// Text
+$_['text_extension'] = 'Extensions';
+$_['text_success'] = 'Success: You have modified Stripe account details!';
+$_['text_edit'] = 'Edit Stripe';
+$_['text_authorization'] = 'Authorization';
+$_['text_sale'] = 'Sale';
+$_['text_stripe'] = '<a target="_BLANK" href="https://stripe.com/"><img src="view/image/payment/stripe.png" alt="Stripe" title="Stripe" style="border: 1px solid #EEEEEE; max-width:100px;" /></a>';
+$_['text_confirm_refund'] = 'Are you sure you want to refund the payment?';
+$_['text_refund_ok'] = 'Refund was successful, order status updated to refunded.';
+
+// Entry
+$_['entry_username'] = 'API Username';
+$_['entry_password'] = 'API Password';
+$_['entry_signature'] = 'API Signature';
+$_['entry_test'] = 'Test Mode';
+$_['entry_transaction'] = 'Transaction Method:';
+$_['entry_total'] = 'Total';
+$_['entry_card'] = 'Store Cards';
+$_['entry_order_status'] = 'Order Status';
+$_['entry_geo_zone'] = 'Geo Zone';
+$_['entry_status'] = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+
+// Help
+$_['help_test'] = 'Use the live or testing (sandbox) gateway server to process transactions?';
+$_['help_total'] = 'The checkout total the order must reach before this payment method becomes active';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Stripe!';
+$_['error_username'] = 'API Username Required!';
+$_['error_password'] = 'API Password Required!';
+$_['error_signature'] = 'API Signature Required!';
+
+// Button
+$_['button_refund'] = 'Refund';
